803740e3d6a2bfce13fc8efca14fdfa106b8e4dc
rick <lurker2000wxc@gmail.com>
2018-03-01 15:21:46 +1100

    ant design added!

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,34 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {  Layout } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+import 'antd/dist/antd.css';
+
+
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
+  <Layout>
+    <Header className="header">
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Layout style={{ padding: '24px 0', background: '#fff' }}>
+        <Sider width={200} style={{ background: '#fff' }}>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>
         Greeting: {this.props.greeting}
+		zzz
+    </Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+    </Footer>
+  </Layout>
+
+
+
       </React.Fragment>
     );
   }
